fec7501d1c7b1bbcd96576cac6468f1416c91209
Felipe Lalanne <felipe@balena.io>
2022-03-13 22:39:15 -0300

    Add minimal tmux configuration to home-manager
    Change-type: minor

--- home.nix
+++ home.nix
@@ -4,16 +4,47 @@
   # Do not delete these lines
   home.username = "me";
   home.homeDirectory = "/home/me";
+
+  # Additional packages to install
+  home.packages = [
+    # pkgs is the set of all packages in the default home.nix implementation
+    # pkgs.nodejs-14_x
+    # pkgs.curl
+  ];
   
   # Add your programs and configurations here 
   programs.tmux = {
     enable = true;
+
     # Use same shortcut as screen
     shortcut = "a";
+
+    # Resize to the size of the smallest session
+    aggressiveResize = true;
+
+    # Time in milliseconds for which tmux waits after an Esc
+    escapeTime = 0;
+
+    # Terminal type    
+    terminal = "screen-256color";
+
+    # Additional configurations
+    extraConfig = ''
+      # split panes using | and -
+      bind | split-window -h
+      bind - split-window -v
+      unbind '"'
+      unbind %
+
+      set -g mouse on                           # enable mouse support
+      set -g bell-action none                   # disable bell
+      setw -g xterm-keys on                     # enable xterm keys
+      set -g status-right '#(date +"%b %_d %H:%M") '
+    '';
   };
 
-  # Link configuration files
-  # home.file.".gitconfig".source = ./vimrc;
+  # Link other configuration files
+  # home.file.".vimrc".source = ./vimrc;
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
