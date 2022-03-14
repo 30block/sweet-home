e5ff25ca8667d30a65caeb994e8dfa3a7b0a3243
Felipe Lalanne <felipe@balena.io>
2022-03-14 12:35:48 -0300

    Add a NIXPKGS_REPO_BRANCH env var
    Change-type: minor

--- entry.sh
+++ entry.sh
@@ -8,11 +8,19 @@ else
 	cmd="$*"
 fi
 
+# Use $@ to manage extra git arguments
+set -- --recurse-submodules -j8
+
+# Repo branch
+NIXPKGS_REPO_BRANCH=${NIXPKGS_REPO_BRANCH:-""}
+if [ -n "${NIXPKGS_REPO_BRANCH}" ]; then
+	set -- "$@" -b "${NIXPKGS_REPO_BRANCH}"
+fi
+
 # If a repository was provided, replace default config with the repo config
 if [ -n "${NIXPKGS_REPO_URL}" ] && [ ! -d "${HOME}/.config/nixpkgs/.git" ]; then
 	rm -rf "${HOME}/.config/nixpkgs"
-	git clone --recurse-submodules -j8 "${NIXPKGS_REPO_URL}" "${HOME}/.config/nixpkgs"
-
+	git clone "$@" "${NIXPKGS_REPO_URL}" "${HOME}/.config/nixpkgs"
 fi
 
 # Update channel definitions and install extra dependencies
