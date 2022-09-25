2e40397428ca1b950c74b96821500e80655b7247
Felipe Lalanne <felipe@balena.io>
2022-09-25 19:54:57 -0300

    Add ssh server
    This adds the openssh-server package to the default configuration. If
    the `SSH_AUTHORIZED_KEYS` environment variable is defined, the
    entrypoint will setup and launch the ssh server for direct access.
    
    Change-type: minor

--- entry.sh
+++ entry.sh
@@ -8,6 +8,47 @@ else
 	cmd="$*"
 fi
 
+# Add environment variables from the docker image and
+# balena to /etc/env, this will ensure that users accessing
+# the shell from running the image directly, using ssh or docker exec
+# have the same experience.
+exclude="HOME PWD USER HOSTNAME SHLVL SSH_AUTHORIZED_KEYS OLDPWD SHELL"
+awk "END { 
+			for (name in ENVIRON) {
+				if (\"${exclude}\" !~ name) {
+					print \"export \"name\"='\"ENVIRON[name]\"'\";
+				}
+			}
+    }" </dev/null | sudo tee /etc/env >/dev/null
+echo "export SWEET_HOME_VERSION=$(cat </etc/version)" | sudo tee -a /etc/env >/dev/null
+
+# If the user set authorized keys then launch sshd
+SSH_SERVER_PORT=${SSH_SERVER_PORT:-2222}
+SSH_SERVER_LOGS=${SSH_SERVER_LOGS:-0}
+if [ -n "${SSH_AUTHORIZED_KEYS}" ]; then
+	# Setup the server keys
+	if [ ! -d "${HOME}/.config/etc/ssh" ]; then
+		mkdir -p "${HOME}/.config/etc/ssh"
+		ssh-keygen -A -f "${HOME}/.config"
+	fi
+
+	if [ ! -d "${HOME}/.ssh" ]; then
+		mkdir -p "${HOME}/.ssh"
+		chmod 0700 "${HOME}/.ssh"
+	fi
+
+	# Write the keys
+	echo "${SSH_AUTHORIZED_KEYS}" | tr "," "\n" >"${HOME}/.ssh/authorized_keys"
+	chmod 0600 "${HOME}/.ssh/authorized_keys"
+
+	# Start the server. By default it drops any output
+	if [ "${SSH_SERVER_LOGS}" != "1" ]; then
+		/usr/sbin/sshd -D -e -p "${SSH_SERVER_PORT}" >/dev/null 2>&1 &
+	else
+		/usr/sbin/sshd -D -e -p "${SSH_SERVER_PORT}" &
+	fi
+fi
+
 # Use $@ to manage extra git arguments
 set -- --recurse-submodules -j8
 
